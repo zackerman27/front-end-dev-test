c72af949c14e4315676585f0ceb25d23d72ab17d
zackerman27 <zackerman27@gmail.com>
2017-09-24 13:51:10 -0400

    Add files via upload

---
+++ phoneNumber.js
@@ -0,0 +1,18 @@
+//
+// Phone Number
+// Finish the rest of 'phoneNumber()' function to satisfy the test
+// file located @ /test/main_test.spec.js
+//
+// To pass, your function should:
+// remove parenthesis, spaces, and hyphens as in "(321) 321-4321"
+// remove dots as in "321.321.4321"
+
+const phoneNumber = (pNum) => {
+// I sincerely wish I had understood this but being entirely unfamiliar with Node.js I feel as though I ran into some errors that I couldn't understand. I will submit my HTML/CSS work and hope that you will still keep me in consideration. I would be very grateful for the opportunity to learn from your developers.
+}
+
+module.exports = phoneNumber
+// Note: It's not neccessary to have all code into the 'phoneNumber'
+// function. Feel free to make as many functions as you see
+// fit. Just be sure to leave phoneNumber as an exported
+// method as you found it
